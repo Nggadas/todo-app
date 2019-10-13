61406832a64f925786dbc1dd99d12221224db210
Emmanuel <enggadas80@gmail.com>
2019-10-13 19:57:48 +0100

    Edited left column content and styling

--- includes/left_column.php
+++ includes/left_column.php
@@ -1,1 +1,22 @@
-sdfasdf
+<div class="future">
+    <h1>Filters</h1>
+    <span class="filters" id="today">
+        <i class="fa fa-calendar-day"></i>
+        Today
+    </span>
+    <span class="filters" id="upcoming">
+        <i class="fa fa-calendar-alt"></i>
+        Upcoming
+    </span>
+</div>
+
+<div class="past">
+    <span class="filters" id="completed">
+        <i class="fa fa-calendar-check"></i>
+        Completed
+    </span>
+    <span class="filters" id="past-due">
+        <i class="fa fa-calendar-times"></i>
+        Past Due Date
+    </span>
+</div>
