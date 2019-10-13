d1ddeec72010447672adb7e05dc5d6445fe84098
Emmanuel <enggadas80@gmail.com>
2019-10-13 19:01:01 +0100

    Created header

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<header>
+    <nav class="navbar">
+        <div class="container header">
+            <a href="/" class="logo">TODO APP</a>
+            <div class="account">
+                <a href="#" class="link">Login or register</a>
+            </div>
+        </div>
+    </nav>
+</header>
+<div class="create">
+    <div class="container">
+        <form action="">
+            <label for="create-task">
+                <input id="create-task" type="text" placeholder="Add new task" autocomplete="off" autofocus>
+            </label>
+            <button id="submit-task">
+                Add
+                <i class="fa fa-plus-circle"></i>
+            </button>
+        </form>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="/assets/images/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Kalam|Lobster|Mansalva&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/fontawesome/all.min.css">
+    <script src="/assets/js/script.js"></script>
+    <script src="/assets/js/fontawesome/all.min.js"></script>
+    <title>TODO App</title>
+</head>
+<body>
+    <?php include 'includes/header.php'?>
+    <div class="container">
+        <div class="left-column">
+            <?php include 'includes/left_column.php'?>
+        </div>
+        <div class="main-content">
+            <?php include 'includes/left_column.php'?>
+        </div>
+    </div>
+</body>
+</html>
