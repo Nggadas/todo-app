a1e913e64a85f95e198de7f27802318fbaa9d85f
Emmanuel <enggadas80@gmail.com>
2019-12-09 16:18:50 +0000

    Edited files to test connection to heroku.

--- includes/connect.php
+++ includes/connect.php
@@ -5,8 +5,9 @@ $servername = $_ENV["SEVER_NAME"];
 $dbname = $_ENV["DB_NAME"];
 $username = $_ENV["USERNAME"];
 $password = $_ENV["PASSWORD"];
+$port = 3306;
 
-$connect = mysqli_connect($servername,$username,$password,$dbname);
+$connect = mysqli_connect($servername,$username,$password,$dbname,$port);
 
 if (mysqli_connect_error()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
